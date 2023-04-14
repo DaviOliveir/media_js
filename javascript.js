fb577eca1a124faf738f4189253ac6aac2e46977
Davi José batista Lima de Oliveira <112220168+DaviOliveir@users.noreply.github.com>
2023-04-13 21:13:08 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+let nota1 = parseFloat(window.prompt('digite o 1 numero'));
+let nota2 = parseFloat(window.prompt('digite o 2 numero'));
+let nota3 = parseFloat(window.prompt('digite o 3 numero'));
+let nota4 = parseFloat(window.prompt('digite o 4 numero'));
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+window.alert(`a média é: ${media.toFixed(2)}`)
